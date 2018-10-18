da755cd1c16c94f582124db046830dc71810330e
michelle <641052383@qq.com>
2018-10-18 10:02:07 +0800

    leetcode

---
+++ 443-压缩字符串.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+
+def main():
+    chars = ["a","a","b","b","c","c","c"]
+    myResult = Solution()
+    print(myResult.compress(chars))
+
+if __name__ == '__main__':
+    main()
