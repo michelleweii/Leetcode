82a39f3dabbd0e1f0d528396df346a5f6f04ed39
michelle <641052383@qq.com>
2018-10-29 20:47:10 +0800

    leetcode

--- 567-字符串的排列.py
+++ 567-字符串的排列.py
@@ -6,34 +6,36 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        # 变成s1中的字母排列组合个数了。。。我的思路
-        # 只要保证s1中的各个字母个数在s2的子串中相同即可
-        c = list(set(s1))
-        for i in c:
-            if s1.count(i) != s2.count(i):
-                return False
-        # 还要判断在s2中是否是连在一起的——判断是子串。
-        start = 0
-        end = 3
-        rev_s2 = s2[::-1]
-        print(rev_s2)
-        for i in range(len(s2)):
-            if s2[i] in c:
-                start = i
-            if rev_s2[i] in c:
-                end = len(s2)-i
+        
 
-        sub_s2 = s2[start]
 
-        print(sub_s2)
+
+    # def checkInclusion(self, s1, s2):
+    #     """
+    #     :type s1: str
+    #     :type s2: str
+    #     :rtype: bool
+    #     """
+    #     # 变成s1中的字母排列组合个数了。。。我的思路
+    #     # 只要保证s1中的各个字母个数在s2的子串中相同即可
+    #     c = list(set(s1))
+    #     # 还要判断在s2中是否是连在一起的——判断是子串。
+    #     sub_s2 = ""
+    #     for i in range(len(s2)):
+    #         if s2[i] in c:
+    #             sub_s2 = s2[i:i+len(s1)]
+    #             # 用字典统计
+    #                 return True
+    #     return False
+
 
 
 
 
 
 def main():
-    s1 = "ab"
-    s2 = "eidbaooo"
+    s1 = "hello"
+    s2 = "ooolleoooleh"
     myResult = Solution()
     # 第一个字符串的排列之一是第二个字符串的子串
     print(myResult.checkInclusion(s1, s2))
