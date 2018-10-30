8696443218bdc327bfba8dc520f9e02cb9920faf
michelle <641052383@qq.com>
2018-10-30 16:06:53 +0800

    leetcode

--- 594-最长和谐子序列.py
+++ 594-最长和谐子序列.py
@@ -1,9 +1,24 @@
+# 输入: [1,3,2,2,5,2,3,7]
+# 输出: 5
+# 原因: 最长的和谐数组是：[3,2,2,2,3].
+import collections
 class Solution(object):
     def findLHS(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        ans = 0
+        dict_nums = collections.Counter(nums)
+        print(dict_nums) # Counter({2: 3, 3: 2, 1: 1, 5: 1, 7: 1})
+        for num in nums:
+            if num+1 in dict_nums:
+                # print(dict_nums[num])
+                # print(dict_nums[num+1])
+                ans = max(ans, dict_nums[num]+dict_nums[num+1])
+                # print(ans)
+        return ans
+
 
 def main():
     nums = [1,3,2,2,5,2,3,7]
