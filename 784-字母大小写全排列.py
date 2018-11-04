74869047e5520f0cd98d1936f136fa938d4ee89f
michelle <641052383@qq.com>
2018-11-04 23:13:44 +0800

    leetcode

---
+++ 784-字母大小写全排列.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def letterCasePermutation(self, S):
+        """
+        :type S: str
+        :rtype: List[str]
+        """
+
+
+def main():
+    S = "a1b2"
+    myResult = Solution()
+    # 第一个字符串的排列之一是第二个字符串的子串
+    print(myResult.letterCasePermutation(S))
+
+if __name__ == '__main__':
+    main()
