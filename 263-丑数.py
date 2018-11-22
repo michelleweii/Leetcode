ab2759bfb53fa0fb9afe01bf1fa356e650bb12e2
michelle <641052383@qq.com>
2018-11-22 12:50:46 +0800

    leetcode

---
+++ 263-丑数.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+
+if __name__ == '__main__':
+    num = 14
+    ans = Solution()
+    print(ans.isUgly(num))
