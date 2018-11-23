c3788408e599a151480afcbd0e6b7831af32e28b
michelle <641052383@qq.com>
2018-11-23 21:06:13 +0800

    leetcode

--- 263-丑数.py
+++ 263-丑数.py
@@ -4,6 +4,19 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
+        while num>1:
+            if num%2==0:
+                num = num//2
+            elif num%3==0:
+                num = num//3
+            elif num%5==0:
+                num = num//5
+            else:
+                break
+        if num==1:
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
     num = 14
