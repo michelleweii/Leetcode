b1a46af00de83b4ab2544ae53247f10c26f8647a
michelle <641052383@qq.com>
2018-12-21 17:30:56 +0800

    leetcode

---
+++ 893-特殊等价字符串组.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def numSpecialEquivGroups(self, A):
+        """
+        :type A: List[str]
+        :rtype: int
+        """
+
+
+
+def main():
+    A = ["a", "b", "c", "a", "c", "c"]
+    myresult = Solution()
+    print(myresult.numSpecialEquivGroups(A))
+
+
+
+if __name__ == "__main__":
+    main()
