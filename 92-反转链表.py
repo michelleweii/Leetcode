e270a66b558ec72d34fad32ea89b4fff790bdc90
michelle <641052383@qq.com>
2018-12-27 14:23:18 +0800

    leetcode

--- 92-反转链表.py
+++ 92-反转链表.py
@@ -5,12 +5,13 @@
 #         self.next = None
 
 class Solution(object):
-    def removeNthFromEnd(self, head, n):
+    def reverseBetween(self, head, m, n):
         """
         :type head: ListNode
+        :type m: int
         :type n: int
         :rtype: ListNode
         """
-
-
+        while(head and --m):
+            # 其实标记一个变量的方法，对我来说可能更好理解
 
