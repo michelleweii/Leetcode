2e144cef0cf5b3bcd5e7ddd9cfb37b6abebf8090
michelle <641052383@qq.com>
2019-01-06 21:30:16 +0800

    leetcode

--- 19-删除链表的倒数第N个节点.py
+++ 19-删除链表的倒数第N个节点.py
@@ -5,6 +5,23 @@ class ListNode(object):
         self.next = None
 
 class Solution(object):
+    def removeNthFromEnd1(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+        p1 = p2 = dummy
+        for i in range(n):
+            p1 = p1.next
+        while p1.next:
+            p1 = p1.next
+            p2 = p2.next
+        p2.next = p2.next.next
+        return dummy.next
+
     def removeNthFromEnd(self, head, n):
         """
         :type head: ListNode
@@ -48,6 +65,7 @@ class Solution(object):
             cur = cur.next
             cnt+=1
         return cnt
+
 
 
 
