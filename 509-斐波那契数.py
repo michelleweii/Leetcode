377314289f42da44d33712917fd6664746509727
michelle <641052383@qq.com>
2019-01-08 20:21:31 +0800

    leetcode

---
+++ 509-斐波那契数.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+
+
+
+
+if __name__ == '__main__':
+    N = 4
+    print(Solution().fib(N))
