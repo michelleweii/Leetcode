b8eb574561edb589c9117bfbeac450c5fda7f2f1
michelle <641052383@qq.com>
2019-02-05 14:20:48 +0800

    leetcode

---
+++ 455-分发饼干.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+
+
+if __name__ == '__main__':
+    g = [1,2,3]
+    s = [1,1]
+    print(Solution().findContentChildren(g,s))
