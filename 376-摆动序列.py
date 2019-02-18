0e33247646a7c3c138550a7acf4784c4842aeed4
michelle <641052383@qq.com>
2019-02-18 20:26:30 +0800

    leetcode

--- 376-摆动序列.py
+++ 376-摆动序列.py
@@ -4,8 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
+        # 在连续递增，递减时，只保留首尾元素
         
+
+
 
 if __name__ == '__main__':
     nums = [1,2,3,4,5,6,7,8,9]
---
+++ 402-移掉K位数字.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeKdigits(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+
+
+
+if __name__ == '__main__':
+    num = "1432219"
+    k = 3
+    print(Solution().removeKdigits(num,k))
