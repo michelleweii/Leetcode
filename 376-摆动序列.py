3cf98dd0c5a9d4200a609c65629b71fc9c75f5c4
michelle <641052383@qq.com>
2019-02-18 19:50:48 +0800

    leetcode

---
+++ 376-摆动序列.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def wiggleMaxLength(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        
+
+if __name__ == '__main__':
+    nums = [1,2,3,4,5,6,7,8,9]
+    print(Solution().wiggleMaxLength(nums))
