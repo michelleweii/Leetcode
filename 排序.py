8d9b632a04e84c97b0265d13636b7cd17043c218
michelle <641052383@qq.com>
2019-02-20 17:40:08 +0800

    sort

--- 排序.py
+++ 排序.py
@@ -20,8 +20,21 @@ def QuickSort(nums,l,r):
         QuickSort(nums,i+1,r)
     return nums
 
+def BubbleSort(nums):
+    n = len(nums)-1
+    for i in range(n,-1,-1): # 控制未排序的长短，已经冒上来的（位于最后）不再做处理
+        flag = 0
+        for j in range(1,i+1,1): # 对未排序的部分，进行两两交换比较
+            if(nums[j-1]>nums[j]):
+                nums[j-1],nums[j] = nums[j],nums[j-1]
+                flag = 1
+
+        if(flag==0): # 没有数据进行交换
+            return nums
+
 
 
 if __name__ == '__main__':
-    nums = [49,38,65,97,76,13,27,49]
-    print(QuickSort(nums,0,len(nums)-1))
+    nums = [49,38,65,97,76,13,27,49,100]
+    # print(QuickSort(nums,0,len(nums)-1))
+    print(BubbleSort(nums))
