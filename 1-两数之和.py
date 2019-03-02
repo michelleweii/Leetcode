a5987a01a48dbb143ace13d78df68a5b15f6bd83
michelle <641052383@qq.com>
2019-03-02 19:40:42 +0800

    leetcode

---
+++ 1-两数之和.py
@@ -0,0 +1,20 @@
+
+# 要求时间复杂度O(N)
+
+# Python solution using hash
+class Solution(object):
+    def twoSum(self, nums, target):
+        if len(nums) <= 1:
+            return False
+        buff_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in buff_dict:
+                return [buff_dict[nums[i]], i]
+            else:
+                buff_dict[target - nums[i]] = i
+
+
+if __name__ == '__main__':
+    nums = [2, 7, 11, 15]
+    target = 9
+    print(Solution().twoSum(nums,target))
