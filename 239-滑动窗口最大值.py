be91e8b586568315a3814a0d64fc1870adf8c1d1
michelle <641052383@qq.com>
2019-03-02 13:40:06 +0800

    leetcode

---
+++ 239-滑动窗口最大值.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+
+
+
+
+        
+if __name__ == '__main__':
+    nums = [1, 3, -1, -3, 5, 3, 6, 7]
+    k = 3
+    print(Solution().maxSlidingWindow(nums,k))
