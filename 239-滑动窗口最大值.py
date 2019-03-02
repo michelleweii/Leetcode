ef90b44e9db54fae213657e3010b1e59b8ec41c7
michelle <641052383@qq.com>
2019-03-02 16:20:33 +0800

    leetcode

--- 239-滑动窗口最大值.py
+++ 239-滑动窗口最大值.py
@@ -9,8 +9,34 @@ class Solution(object):
 
 
 
-        
+
 if __name__ == '__main__':
     nums = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
     print(Solution().maxSlidingWindow(nums,k))
+
+
+import random
+from sklearn import datasets
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+%matplotlib inline
+
+
+# 正规化数据集 X
+def normalize(X, axis=-1, p=2):
+    lp_norm = np.atleast_1d(np.linalg.norm(X, p, axis))
+    lp_norm[lp_norm == 0] = 1
+    return X / np.expand_dims(lp_norm, axis)
+
+
+# 计算一个样本与数据集中所有样本的欧氏距离的平方
+def euclidean_distance(one_sample, X):
+    one_sample = one_sample.reshape(1, -1)
+    X = X.reshape(X.shape[0], -1)
+    distances = np.power(np.tile(one_sample, (X.shape[0], 1)) - X, 2).sum(axis=1)
+    return distances
+
+
+
