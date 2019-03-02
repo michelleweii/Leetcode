4ebbb2d4fd7ea8c614138a18078ff12d1f4e22b5
michelle <641052383@qq.com>
2019-03-02 21:23:28 +0800

    leetcode

--- 剑指offer/面试题3-数字中重复的数字.py
+++ 剑指offer/面试题3-数字中重复的数字.py
@@ -1,6 +1,6 @@
 # 《剑指offer》面试题3
 # using hash
-def find(nums):
+def FindHash(nums):
     n = len(nums)
     nums_dict={}
     for i in range(n):
@@ -10,6 +10,18 @@ def find(nums):
             return nums[i]
     return -1
 
+def FindExchange(nums):
+    pass
+
+# 题目二：不修改数组找出重复的数字
+def Find2(nums):
+    pass
+
+
 if __name__ == '__main__':
     nums = [2,3,1,0,2,5,3]
-    print(find(nums))
+    print("第一题:")
+    print(FindHash(nums))
+    print(FindExchange(nums))
+    print("第二题:")
+    print(Find2(nums))
