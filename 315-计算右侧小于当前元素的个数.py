fc2a47756c79f33aa0b1478eebef155a69060c4e
michelle <641052383@qq.com>
2019-03-03 11:41:50 +0800

    leetcode

---
+++ 315-计算右侧小于当前元素的个数.py
@@ -0,0 +1,14 @@
+# 思路是归并排序
+
+class Solution(object):
+    def countSmaller(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        pass
+
+
+if __name__ == '__main__':
+    nums = [5,2,6,1]
+    print(Solution().countSmaller(nums))
--- 剑指offer/面试题3-数字中重复的数字.py
+++ 剑指offer/面试题3-数字中重复的数字.py
@@ -2,6 +2,8 @@
 # using hash
 def FindHash(nums):
     n = len(nums)
+    if n==0:
+        return -1
     nums_dict={}
     for i in range(n):
         if nums[i] not in nums_dict:
@@ -20,8 +22,10 @@ def Find2(nums):
 
 if __name__ == '__main__':
     nums = [2,3,1,0,2,5,3]
+    # nums = []
     print("第一题:")
     print(FindHash(nums))
     print(FindExchange(nums))
+    print("----------------")
     print("第二题:")
     print(Find2(nums))
