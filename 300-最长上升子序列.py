a08db5840993bc9d9bf95875d221adb5d6318c3c
michelle <641052383@qq.com>
2019-03-09 17:45:45 +0800

    leetcode

---
+++ 300-最长上升子序列.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+
+
+
+if __name__ == '__main__':
+    nums = [10,9,2,5,3,7,101,18]
+    print(Solution().lengthOfLIS(nums))
