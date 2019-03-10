9d2596c1b9a88f040b66f57d2a594296ea38e514
michelle <641052383@qq.com>
2019-03-10 11:55:20 +0800

    leetcode

--- 300-最长上升子序列.py
+++ 300-最长上升子序列.py
@@ -4,8 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
-
+        # dp[i]是以第i个元素结尾的最长上升子序列
+        if len(nums)==0:
+            return 0
+        dp = [1 for _ in range(len(nums))]
+        for i in range(1,len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]: # 当前元素要比之前的元素大，才可以跟在后面，构成上升
+                    if dp[i] < dp[j]+1:
+                        dp[i] = dp[j]+1
+            print(dp[i])
+        return max(dp)
 
 
 if __name__ == '__main__':
