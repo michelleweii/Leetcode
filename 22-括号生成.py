57b9021ad489078b9820859da9dbdb712a86771e
michelle <641052383@qq.com>
2019-03-13 19:36:22 +0800

    leetcode

---
+++ 22-括号生成.py
@@ -0,0 +1,14 @@
+# 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        
+
+
+
+if __name__ == '__main__':
+    n = 3
+    print(Solution().generateParenthesis(n))
