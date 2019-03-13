bd071c7a57fdc284e16f21ddf1b1eff5a467c5ab
michelle <641052383@qq.com>
2019-03-13 13:54:41 +0800

    leetcode

---
+++ 33-搜索旋转排序数组.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+
+
+if __name__ == '__main__':
+    nums = [4, 5, 6, 7, 0, 1, 2]
+    target = 0
+    print(Solution().search(nums,target))
---
+++ 搜索/35-搜索插入位置.py
@@ -0,0 +1,25 @@
+# 二分查找
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        low = 0
+        high = len(nums)-1
+        while(low <= high):
+            mid = (low+high)//2
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                high = mid-1
+            else:
+                low = mid+1
+        # 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+        return low
+
+if __name__ == '__main__':
+    nums = [1, 3, 5, 6]
+    target = 2
+    print(Solution().searchInsert(nums, target))
