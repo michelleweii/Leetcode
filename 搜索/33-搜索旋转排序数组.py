337190d4ca184e5b8d73e3bb834e868bfaab6871
michelle <641052383@qq.com>
2019-03-14 00:37:43 +0800

    leetcode

--- 搜索/33-搜索旋转排序数组.py
+++ 搜索/33-搜索旋转排序数组.py
@@ -9,13 +9,37 @@ class Solution(object):
         low = 0
         high = len(nums)-1
         while low <= high:
-            mid = (mid+high)//2
+            mid = (low+high)//2
             if nums[mid]==target:
                 return mid
+
+            elif target<nums[mid]:
+                if nums[low]<nums[mid]:
+                    if target >= nums[low]:
+                        high = mid-1
+                    else:
+                        low = mid+1
+                elif nums[low] > nums[mid]:
+                    high = mid-1
+                elif nums[low]==nums[mid]:
+                    low = mid+1
+
+            elif target>nums[mid]:
+                if nums[low] < nums[mid]:
+                    low = mid+1
+                elif nums[low] > nums[mid]:
+                    if target>=nums[low]:
+                        high = mid-1
+                    else:
+                        low = mid+1
+                elif nums[low]==nums[mid]:
+                    low = mid+1
+
+        return -1
 
 
 
 if __name__ == '__main__':
-    nums = [4, 5, 6, 7, 0, 1, 2]
-    target = 0
+    nums = [5,1,2,3,4]
+    target = 1
     print(Solution().search(nums,target))
