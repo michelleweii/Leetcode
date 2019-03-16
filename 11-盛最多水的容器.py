5a097d6caaff5114d64ef2482c4e275da15e09d5
michelle <641052383@qq.com>
2019-03-17 00:19:59 +0800

    leetcode

---
+++ 11-盛最多水的容器.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+
+
+if __name__ == '__main__':
+    height = [1,8,6,2,5,4,8,3,7]
+    print(Solution().maxArea(height))
