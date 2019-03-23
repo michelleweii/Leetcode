029c95b0eaae0e9970e2c7ece817c90a3b7bed65
michelle <641052383@qq.com>
2019-03-23 21:56:45 +0800

    leetcode

---
+++ 动态规划/121-买卖股票的最佳时机.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+if __name__ == '__main__':
+    prices = [7,1,5,3,6,4]
+    print(Solution().maxProfit(prices))
