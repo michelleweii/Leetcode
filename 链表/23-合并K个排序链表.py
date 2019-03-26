96f42938633f5ebad367d0a6c0149b8f513146bb
michelle <641052383@qq.com>
2019-03-26 10:17:19 +0800

    leetcode

--- 链表/23-合并K个排序链表.py
+++ 链表/23-合并K个排序链表.py
@@ -6,15 +6,40 @@ class ListNode(object):
         self.val = x
         self.next = None
 
+from queue import PriorityQueue
+from heapq import heappush,heappop
 class Solution(object):
     def mergeKLists(self, lists):
-        """
-        :type lists: List[ListNode]
-        :rtype: ListNode
-        """
+        dummy = curr = ListNode(None)
+        q = PriorityQueue()
+        for idx,node in enumerate(lists):
+            # 我的妈，链表竟然可以这样遍历
+            # print(idx,node) # TypeError: 'ListNode' object is not iterable
+            # 不可以这样遍历- -
+            if node:
+                q.put((node.val,idx,node))
+        # print(q.get()) # (1, 0, <__main__.ListNode object at 0x105d20518>)
+        while not q.empty():
+            # 最小的元素在链表尾部
+            _,idx,curr.next = q.get()
+            curr = curr.next
+            if curr.next:q.put((curr.next.val,idx,curr.next))
+        return dummy.next
 
+    ## using minheap
+    def mergeKLists2(self, lists):
+        dummy = curr = ListNode(None)
+        heap = []
+        for idx, node in enumerate(lists):
+            if node:
+                heappush(heap, (node.val, idx, node))
 
-
+        while heap:
+            _, idx, curr.next = heappop(heap)
+            curr = curr.next
+            if curr.next:
+                heappush(heap, (curr.next.val, idx, curr.next))
+        return dummy.next
 
 if __name__ == "__main__":
     a = ListNode(1)
@@ -34,5 +59,5 @@ if __name__ == "__main__":
     e.next = f
     g.next = h
 
-    print(Solution().mergeKLists(a))
-
+    lists = [a,d,g]
+    print(Solution().mergeKLists(lists))
