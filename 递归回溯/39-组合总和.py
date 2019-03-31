4f4c58bd00a8c7c06f8a0e9fdf584237ad3900cc
michelle <641052383@qq.com>
2019-04-01 00:28:15 +0800

    leetcode

---
+++ 递归回溯/39-组合总和.py
@@ -0,0 +1,8 @@
+class Solution:
+    def combinationSum(self, candidates,target):
+        pass
+
+if __name__ == '__main__':
+    candidates = [2,3,6,7]
+    target = 7
+    print(Solution().combinationSum(candidates,target))
