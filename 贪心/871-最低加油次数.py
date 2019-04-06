a4dcbe4ccf503ffe64317680fd45bb27ee056311
michelle <641052383@qq.com>
2019-04-06 15:11:03 +0800

    leetcode

---
+++ 贪心/871-最低加油次数.py
@@ -0,0 +1,10 @@
+# 腾讯笔试
+class Solution:
+    def minRefuelStops(self, target,startFuel,stations):
+
+
+        
+
+if __name__ == '__main__':
+    target = 100, startFuel = 10, stations = [[10, 60], [20, 30], [30, 30], [60, 40]]
+    print(Solution().minRefuelStops(target,startFuel,stations))
