e20fabe3f1625ff8bad35e3e0c0f4a6505cfd8c2
weiwenjing <weiwenjing@git100.com>
2019-04-22 11:38:12 +0800

    leetcode

---
+++ 377-组合总和 Ⅳ.py
@@ -0,0 +1,10 @@
+class Solution:
+    def combinationSum4(self, nums, target):
+
+
+    def dfs(self):
+
+if __name__ == '__main__':
+    nums = [1, 2, 3]
+    target = 4
+    print(Solution().combinationSum4(nums,target))
