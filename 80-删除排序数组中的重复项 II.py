97fe663d2d60b85a194bcfff2c7774ef46847a66
weiwenjing <weiwenjing@git100.com>
2019-04-24 13:30:28 +0800

    leetcode

--- 80-删除排序数组中的重复项 II.py
+++ 80-删除排序数组中的重复项 II.py
@@ -4,27 +4,22 @@
 # 27,26，283思路相同
 class Solution:
     def removeDuplicates(self, nums):
-        i = 0 
-        flag = 0
-        while i<len(nums)-2:
-            # flag = 0
-            if nums[i]==nums[i+2]:
-                flag = 1
-                del nums[i+2]
-                # print(nums)
-            if not flag:
-                i = i+2
+        i = 2  # 每个元素最多出现两次
+        lens = len(nums)-1
+        while i<lens:
+            if nums[i]==nums[i-2]:
+                del nums[i]
+                lens -=1
             else:
-                flag = 0
-            # print(i)
+                i += 1
         return len(nums)
             
     def test(self,nums):
         print(nums.count(1)) # 3
 
 if __name__ == "__main__":
-    nums = [1,1,1,2,2,3]
-    # nums = [0,0,1,1,1,1,2,3,3]
+    # nums = [1,1,1,2,2,3]
+    nums = [0,0,1,1,1,1,2,3,3]
     # nums = [1,1,1,1]
     res = Solution()
     print(res.removeDuplicates(nums))
