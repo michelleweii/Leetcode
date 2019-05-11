59fb61f7cf1ae77c9781d41e14860aa51a9b8071
michelle <641052383@qq.com>
2019-05-12 00:00:16 +0800

    leetcode

---
+++ 树与二叉树/116-填充每个节点的下一个右侧节点指针.py
@@ -0,0 +1,10 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
