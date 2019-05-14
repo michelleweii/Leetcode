7549d5727d4baf0fcfe12914cdcf369cd4300cc2
weiwenjing <weiwenjing@git100.com>
2019-05-14 10:18:00 +0800

    leetcode

---
+++ 133-克隆图.py
@@ -0,0 +1,24 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val, neighbors):
+        self.val = val
+        self.neighbors = neighbors
+
+class Solution:
+    def cloneGraph(self, node):
+        collect={}
+        def search(node):
+            if not node:
+                return
+            newnode=Node(node.val,[])
+            collect[node]=newnode
+            for i in node.neighbors:
+                if i not in collect:
+                    newnode.neighbors.append(search(i))
+                else:
+                    newnode.neighbors.append(collect[i])
+            return newnode
+        return search(node)
+
+if __name__ == '__main__':
+
---
+++ 动态规划/134-加油站.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canCompleteCircuit(self, gas, cost):
+        pass
+
+
+if __name__ == '__main__':
+    gas = [1, 2, 3, 4, 5]
+    cost = [3, 4, 5, 1, 2]
+    print(Solution().canCompleteCircuit(gas,cost))
