1120d1d6738695d96800459ffb069f800ff20e4a
michelle <641052383@qq.com>
2019-05-18 01:01:50 +0800

    leetcode

---
+++ 139-单词拆分.py
@@ -0,0 +1,9 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        pass
+
+
+if __name__ == '__main__':
+    s = "leetcode"
+    wordDict = ["leet", "code"]
+    print(Solution().wordBreak(s, wordDict))
---
+++ 150-逆波兰表达式求值.py
@@ -0,0 +1,8 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    tokens = ["2", "1", "+", "3", "*"]
+    print(Solution().evalRPN(tokens))
