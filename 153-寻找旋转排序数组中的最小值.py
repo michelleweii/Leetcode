626e44768a698b9a9b78b07ceee4d5d5b18a663b
weiwenjing <weiwenjing@git100.com>
2019-05-20 19:05:52 +0800

    leetcode

---
+++ 153-寻找旋转排序数组中的最小值.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findMin(self, nums):
+        pass
+
+if __name__ == "__main__":
+    nums = [3,4,5,1,2]
+    print(Solution().findMin(nums))
+    
--- 动态规划/134-加油站.py
+++ 动态规划/134-加油站.py
@@ -7,3 +7,6 @@ if __name__ == '__main__':
     gas = [1, 2, 3, 4, 5]
     cost = [3, 4, 5, 1, 2]
     print(Solution().canCompleteCircuit(gas,cost))
+
+
+# https://leetcode-cn.com/problems/gas-station/
