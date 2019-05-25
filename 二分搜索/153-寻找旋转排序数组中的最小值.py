025855dce57bb12182bb6a01e586b88af128938c
michelle <641052383@qq.com>
2019-05-25 12:27:39 +0800

    leetcode

--- 二分搜索/153-寻找旋转排序数组中的最小值.py
+++ 二分搜索/153-寻找旋转排序数组中的最小值.py
@@ -1,7 +1,18 @@
+# mid < right 表面右边有序 否则左边有序 那么mid > right 则表明最小数肯定在mid 和 right之间
 class Solution:
     def findMin(self, nums):
-        pass
+        low, high = 0, len(nums)
+        while low < high - 1:
+            mid = (low+high)//2
+            if nums[mid-1] > nums[mid]:
+                return nums[mid]
+            elif nums[low] > nums[mid-1]:
+                high = mid
+            else:
+                low = mid
+        return nums[0]
+
 
 if __name__ == "__main__":
-    nums = [3,4,5,1,2]
+    nums = [3, 4, 5, 1, 2]
     print(Solution().findMin(nums))
