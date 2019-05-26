f1a282db0d17210fc30de94221e0fecf5854ce52
michelle <641052383@qq.com>
2019-05-26 14:27:14 +0800

    leetcode

---
+++ DFS与BFS与递归/200-岛屿数量.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numIslands(self, grid):
+
+
+if __name__ == '__main__':
+    grid = [[1, 1, 1, 1, 0],
+            [1, 1, 0, 1, 0],
+            [1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0]]
+    print(Solution().numIslands(grid))
