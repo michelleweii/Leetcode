cd260e1c0c5353e861c052067b617950d27417ee
michelle <641052383@qq.com>
2019-08-27 02:09:25 +0800

    leetcode

---
+++ 二分搜索/287-寻找重复数.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findDuplicate(self, nums):
+        l = 1
+        r = len(nums)-1
+        while l<r:
+            mid = l+r>>1
+            sum = 0
+            for x in nums:
+                if x>=1 and x<=mid:sum+=1
+            if sum>mid:r=mid
+            else:l=mid+1
+        return r
+
+if __name__ == '__main__':
+    nums = [1,3,4,2,2]
+    print(Solution().findDuplicate(nums))
