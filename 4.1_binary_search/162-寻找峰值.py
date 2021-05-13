68321e957dd575aefbffd3ceb3eec0676aec43f1
michelle <641052383@qq.com>
2021-05-14 00:11:42 +0800

    fighting0513

--- 4.1_binary_search/162-寻找峰值.py
+++ 4.1_binary_search/162-寻找峰值.py
@@ -1,37 +1,35 @@
-# 你的解法应该是 O(logN) 时间复杂度的。
-# 上坡必有坡顶，所以二分查找大的那一半一定会有峰值
-
-# 规律一：如果nums[i] > nums[i+1]，则在i之前一定存在峰值元素
-# 规律二：如果nums[i] < nums[i+1]，则在i+1之后一定存在峰值元素
 """
-Conditions:
-     1. array length is 1  -> return the only index 
-     2. array length is 2  -> return the bigger number's index 
-     3. array length is bigger than 2 -> 
-           (1) find mid, compare it with its left and right neighbors  
-           (2) return mid if nums[mid] greater than both neighbors
-           (3) take the right half array if nums[mid] smaller than right neighbor
-           (4) otherwise, take the left half
-"""
+特点：
+# 无两段性的性质，但可以用二分来做
+# 二分95%应用于有两段性性质的区间，本题属于剩下的5%
+思路：
+由于假设左右端点的值为负无穷，所以每次在区间中找出一点，
+其到右端点要么单调递减，要么存在峰值
+左端点到该点要么单调递增，要么存在峰值
+步骤：
+我们每次找出区间中间的点，判断它与它右边的数的大小关系（也可比较与左边的数）
+若nums[mid] < nums[mid+1]说明存在单调递增，则可以判断Mid右边的区间必存在峰值，
+因为只有右边是单调递减才会不存在峰值
+每次筛选出的是一定存在峰值的区间，不代表另一半就没有峰值哦
+若nums[mid] >= nums[mid+1]，不代表右边一定没峰值，只是代表左边一定有峰值
+所以为了节省时间快速找到任意一个峰值，我们把右指针 r = mid
 
+链接：https://www.acwing.com/solution/content/7955/
+"""
 
 class Solution:
-    def findPeakElement(self, nums):
-        n = len(nums)
-        first, last = 0, n-1
-        while first < last:
-            mid = (first+last)//2
-            if nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
-                return mid
-            if nums[mid] < nums[mid+1]:
-                first = mid+1
+    def findPeakElement(self, nums) -> int:
+        l = 0; r = len(nums) - 1
+        while l < r:
+            mid = l + r >> 1
+            if nums[mid] < nums[mid + 1]: # 若当前点比右边小，则峰值在右边
+                l = mid + 1
             else:
-                last = mid-1
-        return first  # cinditions 1,2
-
+                r = mid
+        return r
 
 if __name__ == '__main__':
     # nums = [1, 2, 3, 1]
-    nums = [1, 2]
-    # nums = [1, 2, 1, 3, 5, 6, 4]
+    # nums = [1, 2]
+    nums = [1, 2, 1, 3, 5, 6, 4]
     print(Solution().findPeakElement(nums))
--- README.md
+++ README.md
@@ -4,10 +4,21 @@
 
 - 102.二叉树的层序遍历（已完成）
 
-- 103. [二叉树的锯齿形层序遍历](https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal)  （已完成）
+- 103.[二叉树的锯齿形层序遍历](https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal)  （已完成）
 - 二叉树的右视图
 - 二叉树的层平均值
 - N叉树的前序遍历
 - 在每个树行中找最大值
 - 填充每个节点的下一个右侧节点指针
-- 117. [填充每个节点的下一个右侧节点指针 II](https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii) （已完成，链表+层次遍历）
+- 117.[填充每个节点的下一个右侧节点指针 II](https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii) （已完成，链表+层次遍历）
+    
+
+## 2、二分和单调队列
+
+### 2.1、二分
+- 153.旋转数组最小值0513
+
+- 162.寻找峰值0513
+
+
+### 2.2、单调队列
