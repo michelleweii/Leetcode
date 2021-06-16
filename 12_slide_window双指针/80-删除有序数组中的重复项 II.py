5b09d13e01e7f9817684306e339ca21421c8e0c9
michelleweii <641052383@qq.com>
2021-06-16 21:17:01 +0800

    update

---
+++ 12_slide_window双指针/80-删除有序数组中的重复项 II.py
@@ -0,0 +1,31 @@
+"""
+middle
+双指针
+# 27,26，283思路相同
+
+https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/solution/gong-shui-san-xie-guan-yu-shan-chu-you-x-glnq/
+解法一：
+- 由于是保留 k 个相同数字，对于前 k 个数字，我们可以直接保留
+- 对于后面的任意数字，能够保留的前提是：与当前写入的位置前面的第 k 个元素进行比较，不相同则保留
+
+
+解法二：快慢指针
+"""
+class Solution:
+    def removeDuplicates(self, nums):
+        return self.process(nums, 2) # 函数应返回新长度
+
+    def process(self, nums, k):
+        u = 0
+        for x in nums:
+            if u < k or nums[u - k] != x:
+                nums[u] = x
+                u += 1
+
+        # print(nums, u) # [1, 1, 2, 2, 3, 3] 5
+        return u
+
+
+if __name__ == '__main__':
+    nums = [1,1,1,2,2,3]
+    print(Solution().removeDuplicates(nums))
--- 12_slide_window双指针/88-合并两个有序数组.py
+++ 12_slide_window双指针/88-合并两个有序数组.py
@@ -3,23 +3,41 @@ easy
 双指针双向
 # https://leetcode-cn.com/problems/merge-sorted-array/solution/hua-jie-suan-fa-88-he-bing-liang-ge-you-xu-shu-zu-/
 难死了 easy个锤子
+思路：
+- 标签：从后向前数组遍历
+
+- 因为 nums1 的空间都集中在后面，所以从后向前处理排序的数据会更好，节省空间，一边遍历一边将值填充进去
+
+- 设置指针 len1 和 len2 分别指向 nums1 和 nums2 的有数字尾部，从尾部值开始比较遍历，同时设置指针 len 指向 nums1 的最末尾，每次遍历比较值大小之后，则进行填充
+
+- 当 len1<0 时遍历结束，此时 nums2 中海油数据未拷贝完全，将其直接拷贝到 nums1 的前面，最后得到结果数组
+
+- 时间复杂度：O(m+n)
 """
 class Solution:
     # Do not return anything, modify nums1 in-place instead.
     def merge(self, nums1, m, nums2, n):
-        p, q = 0, 0
-        while p<m and q<n:
-            if nums1[p]<=nums2[q]:
-                p += 1
+        p, q = m-1, n-1
+        last = m+n-1
+        while p>=0 and q>=0:
+            if nums1[p] > nums2[q]:
+                nums1[last] = nums1[p]
+                p -= 1
+                last -= 1
             else:
-                nums[]
+                nums1[last] = nums2[q]
+                q -= 1
+                last -= 1
 
-
+        # 如果p的元素一直比q大的话，nums1先结束。nums2还没有遍历完成。
+        # 直接将nums2的元素放在nums1之前
+        res = nums2[:q+1]+nums1
+        return res[:m+n]
 
 
 if __name__ == '__main__':
-    nums1 = [1,2,3,0,0,0]
-    m = 3
-    nums2 = [2,5,6]
-    n = 3
+    nums1 = [0] #[1,2,3,0,0,0]
+    m = 0#3
+    nums2 = [1]#[2,5,6]
+    n = 1#3
     print(Solution().merge(nums1, m, nums2, n))
