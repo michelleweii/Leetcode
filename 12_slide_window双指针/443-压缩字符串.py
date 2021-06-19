4728da5a53bb59aca05d25248d6799ad4a9331bb
michelleweii <641052383@qq.com>
2021-06-19 23:10:49 +0800

    update

--- 12_slide_window双指针/443-压缩字符串.py
+++ 12_slide_window双指针/443-压缩字符串.py
@@ -1,52 +1,38 @@
 """
-双指针
+middle
+双指针同向，滑动窗口
+利用滑动窗口法，左右指针起点都为原数组的首位。
+实现步骤如下：
+1、不断右移右指针，使窗口不断增大；
+2、当窗口内出现一个不同的元素时，就可以将元素及其数量（等于左右指针之差）更新在数组中，然后让左指针指向右指针；
+3、遍历到最后一个字符时也需要结算，因此将右指针的终点设为数组之外一格。当右指针移到终点时也要更新数组。
 """
-# del[1:3]删除指定区间
 class Solution(object):
-    def compress22(self, chars):
-        # 双指针
-        pass
-
     def compress(self, chars):
-        left = i = 0
-        while i < len(chars):
-            char, length = chars[i], 1
-            while (i + 1) < len(chars) and char == chars[i + 1]:
-                length, i = length + 1, i + 1
-            chars[left] = char
-            if length > 1:
-                len_str = str(length)
-                chars[left + 1:left + 1 + len(len_str)] = len_str
-                left += len(len_str)
-            left, i = left + 1, i + 1
-        return left
+        left, right = 0, 0
+        size = 0
+        n = len(chars)
+        while right <= n:
+            # 遍历完成，或右指针元素不等于左指针元素时，更新数组
+            if right==n or chars[right]!=chars[left]:
+                # 更新数组
+                chars[size] = chars[left]
+                size += 1
+                # 更新计数，当个数大于 1 时才更新
+                if (right - left > 1):
+                    i2str = str(right-left)
+                    for x in i2str:
+                        chars[size] = x
+                        size += 1
 
+                left = right
+            right += 1
 
-    def compress_no_in_place(self, chars):
-        """
-        :type chars: List[str]
-        :rtype: int
-        """
-        char_map = {}
-        for index,val in enumerate(chars):
-            if val not in char_map:
-                char_map[val] = 1
-            else:
-                cnt = char_map[val]
-                cnt += 1
-                char_map[val] = cnt
-        rs = []
-        for key in char_map:
-            rs.append(key)
-            if char_map[key] == 1:
-                continue
-            else:
-                rs.append(str(char_map[key]))
-        print(rs)
-        length = "".join(rs)
-        return len(length)
+        print(chars) # ['a', '2', 'b', '2', 'c', '3', 'c']
+        return size # 6
 
 if __name__ == '__main__':
+    # chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
     chars = ["a","a","b","b","c","c","c"]
     myResult = Solution()
     print(myResult.compress(chars))
--- 12_slide_window双指针/567-字符串的排列.py
+++ 12_slide_window双指针/567-字符串的排列.py
@@ -1,22 +1,63 @@
 """
 middle
 滑动窗口 （与76+438.找到字符串中所有字母异位词相似）
+
+思路：
+flag：记录窗口中满足条件的字符个数
+若一个字符进入窗口，应该增加 window 计数器；若一个字符将移出窗口，应该减少 window 计数器；
+当 flag 满足 tMap 时应收缩窗口；收缩窗口的时候应该更新最终结果。
+当发现某个字符在 window 中的数量满足了 tMap 的需要，就要更新 flag，表示有一个字符已满足要求。
+当 right - left + 1 == t.length()时，窗口大小 "等于" 字符串 t 的长度时移动 left 缩小窗口，
+因为各种排列的长度显然应该是一样的。
+
+当 flag == tMap.size()时，说明窗口中的数据是一个合法的排列，此时立即返回 true。
+
+链接：https://leetcode-cn.com/problems/permutation-in-string/solution/zi-fu-chuan-de-pai-lie-hua-dong-chuang-k-sos8/
 """
 
+# flag：记录窗口中满足条件的字符个数
+# 当发现某个字符在 window 中的数量满足了 tMap 的需要，就要更新 flag，表示有一个字符已满足要求。
+# 当 flag == tMap.size() 时，说明窗口中的数据是一个合法的排列，此时立即返回 true。
+
+# 窗口中可以有其他字符，但是 windowMap 中只存放当前窗口中与要找的字符串 t 中字符相等字符的出现次数
+# S = "EBBANCF"，当窗口下标为 [0, 5] 时，windowMap = {A:1, B:2, C:1}
+# T = "ABC"，其中 tMap = {A:1, B:1, C:1}
 
 from collections import Counter
 class Solution:
+    # s1是短的， s2是长的
     def checkInclusion(self, s1, s2):
-        d1, d2 = Counter(s1), Counter(s2[:len(s1)])
-        for start in range(len(s1), len(s2)):
-            if d1 == d2: return True
-            d2[s2[start]] += 1
-            d2[s2[start - len(s1)]] -= 1
-            print(d2)
-            if d2[s2[start - len(s1)]] == 0:
-                # print(start) # 2 3 4
-                del d2[s2[start - len(s1)]]
-        return d1 == d2
+        n = len(s2)
+        # window 记录窗口中的字符；tMap 记录需要凑齐的字符(need)
+        window_map = {}
+        t_map = Counter(s1)
+
+        right, left = 0, 0
+        flag = 0 # 记录窗口中满足条件的字符个数
+
+        while right < n:
+            # 如果当前字符在t_map中
+            if s2[right] in t_map:
+                # 就将该字符加入到window中
+                window_map[s2[right]] = window_map.get(s2[right],0)+1
+                # 如果这个字符和t_map中一样了，说明有一个字符满足要求了
+                if window_map.get(s2[right],0) == t_map.get(s2[right],0):
+                    flag += 1
+
+            # 寻找最优解
+            # 窗口大小 "等于" 字符串 t 的长度时移动 left 缩小窗口
+            while right-left+1 == len(s1):
+                if flag==len(t_map): return True
+
+                # 字符移除窗口
+                if s2[left] in t_map:
+                    if window_map.get(s2[left],0) == t_map.get(s2[left],0):
+                        flag -= 1
+                    window_map[s2[left]] = window_map.get(s2[left],0)-1
+
+                left += 1
+            right += 1
+        return False
 
 
 if __name__ == '__main__':
