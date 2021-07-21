9da6b3e9483464c2ccbf3d67accff202efb87a61
michelleweii <641052383@qq.com>
2021-07-22 00:18:25 +0800

    update

--- 021_剑指offer二刷/剑指 Offer 43-1～n 整数中 1 出现.py
+++ 021_剑指offer二刷/剑指 Offer 43-1～n 整数中 1 出现.py
@@ -1,5 +1,7 @@
 """
 hard
+https://leetcode-cn.com/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof/solution/python3si-lu-dai-ma-10fen-zhong-jiang-qi-9btr/
+2021-07-22
 """
 class Solution:
     def countDigitOne(self, n: int) -> int:
