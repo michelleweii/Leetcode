1ce648f5b7bb269371bf6e133dcc2a1241025fc0
michelleweii <641052383@qq.com>
2021-12-15 00:45:45 +0800

    updates

---
+++ 17_模拟题/292-Nim 游戏.py
@@ -0,0 +1,12 @@
+"""
+easy 2021-12-
+不懂为什么是腾讯精选50题。。。
+"""
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        pass
+
+
+if __name__ == '__main__':
+    n = 4 # F
+    print(Solution().canWinNim(n))
---
+++ 知识的荒漠/位运算/231-2 的幂.py
@@ -0,0 +1,13 @@
+"""
+easy 2021-12- 位运算
+"""
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        pass
+
+
+
+
+if __name__ == '__main__':
+    n = 16
+    print(Solution().isPowerOfTwo(n))
