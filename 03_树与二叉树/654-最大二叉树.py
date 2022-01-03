a130fcf6a65e485c02ebf33532fdadf91c9d970f
michelleweii <641052383@qq.com>
2022-01-03 21:37:49 +0800

    updates

--- 03_树与二叉树/654-最大二叉树.py
+++ 03_树与二叉树/654-最大二叉树.py
@@ -21,38 +21,8 @@ class Solution:
         # 递归构造右子树
         root.right = self.constructMaximumBinaryTree(nums[index+1:])
         return root
-
-"""
-class Solution {
-private:
-    // 在左闭右开区间[left, right)，构造二叉树
-    TreeNode* traversal(vector<int>& nums, int left, int right) {
-        if (left >= right) return nullptr;
-
-        // 分割点下标：maxValueIndex
-        int maxValueIndex = left;
-        for (int i = left + 1; i < right; ++i) {
-            if (nums[i] > nums[maxValueIndex]) maxValueIndex = i;
-        }
-
-        TreeNode* root = new TreeNode(nums[maxValueIndex]);
-
-        // 左闭右开：[left, maxValueIndex)
-        root->left = traversal(nums, left, maxValueIndex);
-
-        // 左闭右开：[maxValueIndex + 1, right)
-        root->right = traversal(nums, maxValueIndex + 1, right);
-
-        return root;
-    }
-public:
-    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        return traversal(nums, 0, nums.size());
-    }
-};
-"""
     # 单调栈
-
+    
 if __name__ == '__main__':
     nums = [3, 2, 1, 6, 0, 5]
     print(Solution().constructMaximumBinaryTree(nums))
