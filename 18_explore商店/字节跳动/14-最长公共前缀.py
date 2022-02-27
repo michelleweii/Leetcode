e24849553da3d0b54995f189fc24466c840bd587
michelleweii <641052383@qq.com>
2022-02-28 00:32:43 +0800

    updates

--- 18_explore商店/字节跳动/14-最长公共前缀.py
+++ 18_explore商店/字节跳动/14-最长公共前缀.py
@@ -1,24 +1,18 @@
 """
-easy 2021-10-17 纵向遍历
+easy 2022-02-25 纵向遍历
 # python zip特性
 """
 class Solution:
     # 纵向遍历
-    # i 单词的每一位
-    # j 每一个单词
     def longestCommonPrefix(self, strs):
         if not strs:return ""
-        length, count = len(strs[0]), len(strs)
-        for i in range(length): # 第一个单词的长度，遍历第一个单词
-            c = strs[0][i] # 第一个单词的每一个字母
 
-            # if any(i == len(strs[j]) or strs[j][i] != c for j in range(1, count)):
-            for j in range(1,count): # 遍历每一个单词
-                # 当前单词第i位与第一个单词第i位不同；
-                # 或者，已经遍历完成当前单词j
-                # if strs[j][i] != c or i==len(strs[j]):
-                ## 想想 or  and 连接判断语句的语法
-                if i == len(strs[j]) or strs[j][i] != c: ### 【注意】or的前后顺序不可以颠倒
+        lens1, size = len(strs[0]), len(strs)
+        # 先遍历第一个单词
+        for i in range(lens1):
+            for j in range(1, size):
+                print([0][i], strs[j])
+                if i==len(strs[j]) or strs[0][i]!=strs[j][i]:
                     return strs[0][:i]
         return strs[0]
 
